1d5b49893d287c8806d93c4c2174d9cc39470dec
Rui Vieira <ruidevieira@googlemail.com>
2021-10-27 16:09:01 +0100

    Fix some CI errors

--- humble.el
+++ humble.el
@@ -1,3 +1,20 @@
+;;; humble.el --- org-mode SSG
+
+;; Copyright (C) 2021 Rui Vieira
+
+;; Author: Rui Vieira <ruidevieira@googlemail.com>
+;; Version: 0.1
+;; Keywords: python, projects
+;; URL: https://github.com/ruivieira/elisp
+
+;;; Commentary:
+
+;; org-mode SSG
+
+
+;;; Code:
 (require 'org)
 (require 'org-element)
 
+(provide 'humble)
+;;; humble.el ends here
--- kogito.el
+++ kogito.el
@@ -1,3 +1,18 @@
+;;; kogito.el --- Kogito utilities
+
+;; Copyright (C) 2021 Rui Vieira
+
+;; Author: Rui Vieira <ruidevieira@googlemail.com>
+;; Version: 0.1
+;; Keywords: python, projects
+;; URL: https://github.com/ruivieira/elisp
+
+;;; Commentary:
+
+;; A package to manage Kogito projects.
+
+
+;;; Code:
 (setq kogito-apps-root "~/Sync/code/rh/trusty/kogito-apps")
 (defun kogito-make-path (p) (concat kogito-apps-root "/" p))
 (defun maven-install-string (p) (concat "mvn -f " (kogito-make-path p) "/pom.xml clean install -DskipTests\n"))
@@ -12,3 +27,5 @@
 (maven-install "persistence-commons")
 (maven-install "explainability")
 
+(provide 'kogito)
+;;; kogito.el ends here
