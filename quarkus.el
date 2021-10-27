e93dfe9f0cac2a0720314193ce67e73f2ab513d5
Rui Vieira <ruidevieira@googlemail.com>
2021-10-27 11:03:28 +0100

    Add extensions to quarkus package

--- quarkus.el
+++ quarkus.el
@@ -1,4 +1,8 @@
+(require 'request)
+(require 'json)
+
 (setq quarkus-version "2.3.1.Final")
+(setq quarkus--api-url "https://stage.code.quarkus.io/api")
 
 (defun quarkus-create-app ()
   (interactive)
@@ -14,3 +18,41 @@
     -DclassName='%4$s.GreetingResource' \
     -Dpath='/hello'\n" quarkus-version group-id artifact-id namespace))
   )
+
+(defun quarkus--get-extensions ()
+  (request-response-data (request (concat quarkus--api-url "/extensions")
+    :parser 'json-read
+    :sync t
+    ))
+  )
+
+(defun quarkus--maven-add-extension (extension)
+  (comint-send-string
+   (get-buffer-process (shell))
+   (format "./mvnw quarkus:add-extension -Dextensions=\"%s\"\n" extension))
+  )
+
+(defun quarkus-list-extensions ()
+  (interactive)
+  (setq extensions (quarkus--get-extensions))
+  (string-join (mapcar '(lambda (e)
+             (format "%s (%s)"
+             (alist-get 'name e)
+             (alist-get 'version e)))
+          extensions)
+               "\n"))
+
+  (defun quarkus-add-extension ()
+    (interactive)
+
+    (setq extensions (quarkus--get-extensions))
+    (setq ext-table (make-hash-table :test 'equal))
+    (mapcar '(lambda (e)
+               (puthash
+                (format "%s (%s)"
+                       (alist-get 'name e)
+                       (alist-get 'version e))
+                e ext-table))
+            extensions)
+    (quarkus--maven-add-extension (alist-get 'id (gethash (ido-completing-read "Extension to add: " (hash-table-keys ext-table)) ext-table)))
+    )
