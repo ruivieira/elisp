663e94273284b8e14d0ce4cdb8d6c9c23df04db4
Rui Vieira <ruidevieira@googlemail.com>
2021-11-01 22:12:48 +0000

    Add create issue from JIRA

--- agile.el
+++ agile.el
@@ -14,8 +14,11 @@
 
 ;;; Code:
 (require 'org)
+(require 'request)
+(require 'json)
 
 (setq agile/doc-root (expand-file-name "~/Sync/notes/pages/JIRAs/"))
+(setq agile/jira-server "https://issues.redhat.com/")
 
 (defun agile--build-ticket-string (id title)
   (format "#+Title: %1$s-%2$s\n\n\
@@ -24,30 +27,58 @@
 *** JIRA PR %1$s\n" id title)
   )
 
+(defun agile--build-review-string (id title)
+  (format "#+Title: %1$s-%2$s\n\n* REVIEW %1$s\n" id title))
+
 (defun agile--build-ticket-filename (id title)
   (format "%s - %s.org" id title))
 
+(defun agile--fetch-ticket-jira-json (id)
+  (request-response-data (request (concat agile/jira-server "/rest/api/latest/issue/" id)
+                                                    :parser 'json-read
+                                                    :sync t
+                                                    )))
+
+(defun agile--ticket-save (id summary)
+  (write-region (agile--build-ticket-string id summary)
+                nil
+                (concat agile/doc-root
+                        (agile--build-ticket-filename id summary))))
+
 (defun agile-ticket-create ()
+  (interactive)
+  (setq id (read-string "Ticket ID: "))
+  (setq summary (read-string "Ticket title: " "Ticket title"))
+  (agile--ticket-save id summary))
+
+(defun agile-ticket-create-from-jira ()
+  (interactive) 
+  (setq id (read-string "Ticket ID: "))
+  (setq json (agile--fetch-ticket-jira-json id))
+  (agile--ticket-save id (alist-get 'summary (alist-get 'fields json)))
+  )
+
+(defun agile-review-create ()
   (interactive)
   (setq ticket-id (read-string "Ticket ID: "))
   (setq ticket-title (read-string "Ticket title: " "Ticket title"))
-  (write-region (agile--build-ticket-string ticket-id ticket-title)
+  (write-region (agile--build-review-string ticket-id ticket-title)
                 nil
                 (concat agile/doc-root
                         (agile--build-ticket-filename ticket-id ticket-title)))
   )
 
-(setq
- org-agenda-custom-commands
- '(
-   ("l" todo "LATER")
-   ("j" "Agenda and JIRAs" ((agenda "") (todo "JIRA") (todo "REVIEW")))
-   ("m" "Agenda and meetings" ((agenda "") (todo "MEETING")))
-   ("w" "Work tasks and meetings" ((agenda "") (todo "MEETING") (todo "REVIEW") (todo "JIRA") (tags "+work")))
-   ("tw" tags-todo "+work")
-   ("n" "Agenda and all TODOs" ((agenda "") (todo "TODO")))
-   ("f" "Fortnight agenda and all TODOs" ((agenda "" ((org-agenda-span 14))) (alltodo "")))
-   ))
+;; (setq
+;; org-agenda-custom-commands
+;;  '(
+;;   ("l" todo "LATER")
+;;   ("j" "Agenda and JIRAs" ((agenda "") (todo "JIRA") (todo "REVIEW")))
+;;   ("m" "Agenda and meetings" ((agenda "") (todo "MEETING")))
+;;   ("w" "Work tasks and meetings" ((agenda "") (todo "MEETING") (todo "REVIEW") (todo "JIRA") (tags "+work")))
+;;   ("tw" tags-todo "+work")
+;;   ("n" "Agenda and all TODOs" ((agenda "") (todo "TODO")))
+;;   ("f" "Fortnight agenda and all TODOs" ((agenda "" ((org-agenda-span 14))) (alltodo "")))
+;;   ))
 
 (provide 'agile)
 ;;; agile.el ends here
