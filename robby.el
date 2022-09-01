1dd6e0f4ca4d1b07eaefafbf4c8998265e7784ce
Rui Vieira <ruidevieira@googlemail.com>
2022-09-01 23:05:04 +0100

    Add Robby package

---
+++ robby.el
@@ -0,0 +1,29 @@
+;;; robby.el --- Personal automation
+
+;; Copyright (C) 2022 Rui Vieira
+
+;; Author: Rui Vieira <ruidevieira@googlemail.com>
+;; Version: 0.1
+;; Keywords: python, projects, xonsh
+;; URL: https://github.com/ruivieira/elisp
+
+;;; Commentary:
+
+;; A package for personal automation.
+
+
+;;; Code:
+
+(defun robby-notes-move ()
+  "Move the Obsidian notes"
+  (interactive)
+  (compile "/home/rui/Sync/code/robby/robby/xontrib/notes.xsh move"))
+
+(map! :leader
+      (:prefix-map ("r" . "robby")
+       (:prefix ("n" . "notes")
+        :desc "Move notes" "m" #'robby-notes-move
+        :desc "Another entry" "s" #'robby-notes-move)))
+
+(provide 'robby)
+;;; robby.el ends here
